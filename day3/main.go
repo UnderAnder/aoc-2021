c671681974ee743d4d2f2bca985c0938100a68f1
Under Ander <andryushin.alexander@gmail.com>
2021-12-03 23:27:06 +0700

    part 2

--- day3/main.go
+++ day3/main.go
@@ -7,6 +7,13 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+)
+
+const (
+	asciiZero = 48
+	asciiOne  = 49
+	noCommon  = 99
 )
 
 func main() {
@@ -18,10 +25,10 @@ func main() {
 	buf1 := &bytes.Buffer{}
 	buf2 := io.TeeReader(file, buf1)
 	fmt.Printf("First answer: %d\n", part1(buf2))
+	fmt.Printf("Second answer: %d\n", part2(buf1))
 }
 
 func part1(file io.Reader) int {
-	const asciiZero = 48
 	var total int
 	var gamma, epsilon uint16
 	zeroCounter := make([]int, 12, 12)
@@ -45,8 +52,69 @@ func part1(file io.Reader) int {
 			log.Fatal("no most common bit in the corresponding position")
 		}
 	}
-	log.Printf("%.16b", gamma)
-	log.Printf("%.16b", epsilon)
 	answer := int(gamma) * int(epsilon)
 	return answer
+}
+
+func part2(file io.Reader) int {
+	var generator, scrubber string
+	var mComBuf []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		mComBuf = append(mComBuf, scanner.Text())
+	}
+	lComBuf := make([]string, len(mComBuf))
+	copy(lComBuf, mComBuf)
+
+	for i := 0; i < len(mComBuf[0]); i++ {
+		var tmp []string
+		mCom := mostCommon(mComBuf, i)
+		for _, v := range mComBuf {
+			if v[i] == mCom || (v[i] == asciiOne && mCom == noCommon) {
+				tmp = append(tmp, v)
+			}
+		}
+		if len(tmp) == 1 {
+			generator = tmp[0]
+		}
+		mComBuf = tmp
+	}
+
+	for i := 0; i < len(mComBuf[0]); i++ {
+		var tmp []string
+		mCom := mostCommon(lComBuf, i)
+		for _, v := range lComBuf {
+			if v[i] != mCom && mCom != noCommon || (v[i] == asciiZero && mCom == noCommon) {
+				tmp = append(tmp, v)
+			}
+		}
+		if len(tmp) == 1 {
+			scrubber = tmp[0]
+		}
+		lComBuf = tmp
+	}
+	oxy, _ := strconv.ParseInt(generator, 2, 64)
+	co2, _ := strconv.ParseInt(scrubber, 2, 64)
+	answer := oxy * co2
+	return int(answer)
+}
+
+func mostCommon(s []string, pos int) uint8 {
+	var zeros, ones int
+	for _, v := range s {
+		switch v[pos] {
+		case asciiZero:
+			zeros++
+		case asciiOne:
+			ones++
+		}
+	}
+	switch {
+	case zeros > ones:
+		return asciiZero
+	case ones > zeros:
+		return asciiOne
+	default:
+		return noCommon
+	}
 }
