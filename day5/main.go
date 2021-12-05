16fbc08282536bad5da32a0cfac1ca888887d644
Under Ander <andryushin.alexander@gmail.com>
2021-12-05 21:59:10 +0700

    day5 part2 [ugly]

--- day5/main.go
+++ day5/main.go
@@ -20,114 +20,6 @@ type entry struct {
 	p1, p2 point
 }
 
-// func newBoard(nums []int) *board {
-// 	b := new(board)
-// 	k := 0
-// 	for i := 0; i < len(b.cells); i++ {
-// 		for j := 0; j < len(b.cells[i]); j++ {
-// 			b.cells[i][j].num = nums[k]
-// 			k++
-// 		}
-// 	}
-// 	return b
-// }
-
-// func (b *board) mark(num int) (bool, int) {
-// 	for i := 0; i < len(b.cells); i++ {
-// 		for j := 0; j < len(b.cells[i]); j++ {
-// 			if b.cells[i][j].num == num {
-// 				b.cells[i][j].mark = true
-// 				win, score := b.check(num, i, j)
-// 				if win {
-// 					return true, score
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return false, -1
-// }
-
-// func (b *board) check(num, i, j int) (bool, int) {
-// 	win := true
-// 	score := 0
-// 	for k := 0; k < len(b.cells[i]); k++ {
-// 		win = win && b.cells[i][k].mark
-// 	}
-// 	if !win {
-// 		win = true
-// 		for k := 0; k < len(b.cells); k++ {
-// 			win = win && b.cells[k][j].mark
-// 		}
-// 	}
-// 	if win {
-// 		for i = 0; i < len(b.cells); i++ {
-// 			for j = 0; j < len(b.cells[i]); j++ {
-// 				if !b.cells[i][j].mark {
-// 					score += b.cells[i][j].num
-// 				}
-// 			}
-// 		}
-// 		score *= num
-// 	}
-// 	return win, score
-// }
-//
-// func solve(file io.Reader) (int, int) {
-// 	var boards []*board
-// 	var win bool
-// 	var answer, answer1, answer2, winCount int
-// 	boardNums := make([]int, 0, size*size)
-//
-// 	scanner := bufio.NewScanner(file)
-//
-// 	// draws
-// 	scanner.Scan()
-// 	drawsStr := strings.Split(scanner.Text(), ",")
-// 	draws := make([]int, 0, len(drawsStr))
-// 	for _, v := range drawsStr {
-// 		num, err := strconv.Atoi(v)
-// 		if err != nil {
-// 			log.Fatalf("can't parse draw")
-// 		}
-// 		draws = append(draws, num)
-// 	}
-//
-// 	// boards
-// 	for i := 0; scanner.Scan(); i++ {
-// 		for _, v := range strings.Fields(scanner.Text()) {
-// 			num, err := strconv.Atoi(v)
-// 			if err != nil {
-// 				log.Fatalf("can't parse board")
-// 			}
-// 			boardNums = append(boardNums, num)
-// 		}
-// 		if len(boardNums) == size*size {
-// 			boards = append(boards, newBoard(boardNums))
-// 			boardNums = make([]int, 0, size*size)
-// 		}
-// 	}
-//
-// 	// solve
-// 	for _, draw := range draws {
-// 		for i := range boards {
-// 			if boards[i] == nil {
-// 				continue
-// 			}
-// 			if win, answer = boards[i].mark(draw); win {
-// 				winCount++
-// 				boards[i] = nil
-// 				if winCount == 1 {
-// 					answer1 = answer
-// 				}
-// 				if winCount == len(boards) {
-// 					answer2 = answer
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return answer1, answer2
-// }
-
 func parse(file io.Reader) []entry {
 	entries := make([]entry, 0)
 	scanner := bufio.NewScanner(file)
@@ -202,6 +94,98 @@ func part1(entries []entry) int {
 	return answer
 }
 
+func part2(entries []entry) int {
+	var answer int
+	field := make(map[point]int)
+
+	for _, e := range entries {
+		var start, end int
+		if e.p1.x == e.p2.x {
+			start = e.p1.y
+			end = e.p2.y
+			if e.p1.y > e.p2.y {
+				start = e.p2.y
+				end = e.p1.y
+			}
+			for i := start; i <= end; i++ {
+				field[point{e.p2.x, i}]++
+			}
+			continue
+		}
+		if e.p1.y == e.p2.y {
+			start = e.p1.x
+			end = e.p2.x
+			if e.p1.x > e.p2.x {
+				start = e.p2.x
+				end = e.p1.x
+			}
+			for i := start; i <= end; i++ {
+				field[point{i, e.p2.y}]++
+			}
+			continue
+		}
+		if e.p2.y == e.p2.x && e.p1.y == e.p1.x {
+			start, end = e.p1.x, e.p2.x
+			if e.p1.x > e.p2.x {
+				start, end = end, start
+			}
+
+			for i := start; i <= end; i++ {
+				field[point{i, i}]++
+			}
+			continue
+		}
+		if e.p1.x == e.p2.y && e.p1.y == e.p2.x {
+			start, end = e.p1.x, e.p2.x
+			if e.p1.x > e.p2.x {
+				start, end = end, start
+			}
+			j := end
+			for i := start; i <= end; i++ {
+				field[point{j, i}]++
+				j--
+			}
+		}
+		if e.p1.x+e.p2.y == e.p2.x+e.p1.y {
+			start, end = e.p1.x, e.p2.x
+			start2, end2 := e.p1.y, e.p2.y
+			if e.p1.x > e.p2.x {
+				start, end = end, start
+			}
+			if e.p1.y > e.p2.y {
+				start2, end2 = end2, start2
+			}
+			j := start2
+			for i := start; i <= end; i++ {
+				log.Printf("x:%d y:%d", i, j)
+				field[point{i, j}]++
+				j++
+			}
+		}
+		if e.p1.x+e.p1.y == e.p2.x+e.p2.y {
+			start, end = e.p1.x, e.p2.x
+			start2, end2 := e.p1.y, e.p2.y
+			if e.p1.x > e.p2.x {
+				start, end = end, start
+			}
+			if e.p1.y > e.p2.y {
+				start2, end2 = end2, start2
+			}
+			j := end2
+			for i := start; i <= end; i++ {
+				field[point{i, j}]++
+				j--
+			}
+		}
+	}
+	for _, i := range field {
+		if i > 1 {
+			answer++
+		}
+	}
+	return answer
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -211,4 +195,5 @@ func main() {
 	entries := parse(file)
 
 	fmt.Printf("First answer: %d\n", part1(entries))
+	fmt.Printf("Second answer: %d\n", part2(entries))
 }
