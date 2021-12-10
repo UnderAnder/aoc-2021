f2884e0ddafda5cb4ff97516da4f67db0bad9ca3
Under Ander <andryushin.alexander@gmail.com>
2021-12-10 16:30:47 +0700

    add day10 part2

--- day10/main.go
+++ day10/main.go
@@ -6,15 +6,22 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
 )
 
-func solve(lines []string) int {
-	var answer int
+func solve(lines []string) (int, int) {
+	var answer1 int
 	illigalScore := map[rune]int{
 		41:  3,     // )
 		93:  57,    // ]
 		125: 1197,  // }
 		62:  25137, // >
+	}
+	completeScore := map[rune]int{
+		41:  1, // )
+		93:  2, // ]
+		125: 3, // }
+		62:  4, // >
 	}
 	pairTable := map[rune]rune{
 		40:  41,  // ()
@@ -30,6 +37,8 @@ func solve(lines []string) int {
 	}
 
 	illigals := make([]rune, 0)
+	completes := make([]int, 0)
+lines:
 	for _, line := range lines {
 		tmp := make([]rune, 0, 20)
 		for _, char := range line {
@@ -39,19 +48,30 @@ func solve(lines []string) int {
 			}
 			r, ok := reversTable[char]
 			if ok {
+				// part1
 				if tmp[len(tmp)-1] != r {
 					illigals = append(illigals, char)
-					break
+					continue lines
 				}
 				tmp = tmp[:len(tmp)-1]
 			}
 		}
+		// part2
+		score := 0
+		for i := len(tmp) - 1; i >= 0; i-- {
+			r := pairTable[tmp[i]]
+			score *= 5
+			score += completeScore[r]
+		}
+		completes = append(completes, score)
 	}
 
 	for _, char := range illigals {
-		answer += illigalScore[char]
+		answer1 += illigalScore[char]
 	}
-	return answer
+	sort.Ints(completes)
+	answer2 := completes[len(completes)/2]
+	return answer1, answer2
 }
 
 func parse(file io.Reader) []string {
@@ -65,7 +85,7 @@ func parse(file io.Reader) []string {
 }
 
 func main() {
-	file, err := os.Open("test.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -73,6 +93,7 @@ func main() {
 		_ = file.Close()
 	}(file)
 	ps := parse(file)
-	a1 := solve(ps)
+	a1, a2 := solve(ps)
 	fmt.Printf("First answer: %d\n", a1)
+	fmt.Printf("Second answer: %d\n", a2)
 }
