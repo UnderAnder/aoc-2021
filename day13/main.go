5950783e002bb5575df4427b98ae94188c4b39fe
Under Ander <andryushin.alexander@gmail.com>
2021-12-13 17:04:18 +0700

    add day13 part2

--- day13/main.go
+++ day13/main.go
@@ -20,55 +20,60 @@ type point struct {
 	x, y int
 }
 
-func (p point) plus(p2 point) point {
-	return point{p.x + p2.x, p.y + p2.y}
-}
-
-func (g *grid) inBound(p point) bool {
-	if p.x >= g.w || p.x < 0 || p.y >= g.h || p.y < 0 {
-		return false
-	}
-	return true
+type fold struct {
+	x, y int
 }
 
 func (g *grid) String() {
 	for i := 0; i <= g.h; i++ {
 		for j := 0; j <= g.w; j++ {
-			if _, ok := g.field[point{j, i}]; ok {
-				fmt.Printf("#")
+			if _, ok := g.field[point{j, i}]; !ok {
+				fmt.Print(".")
 				continue
 			}
-			fmt.Printf(".")
+			fmt.Print("#")
 		}
 		fmt.Println()
 	}
-	fmt.Println()
 }
 
-func (g *grid) foldY(y int) {
+func (g *grid) fold(fold fold) {
 	for k := range g.field {
-		if k.y > y {
-			py := (y - (k.y % y)) % y
-			g.field[point{k.x, py}] = struct{}{}
-			delete(g.field, k)
+		if fold.x != -1 {
+			if k.x > fold.x {
+				px := (fold.x - (k.x % fold.x)) % fold.x
+				g.field[point{px, k.y}] = struct{}{}
+				delete(g.field, k)
+			}
+		} else {
+			if k.y > fold.y {
+				py := (fold.y - (k.y % fold.y)) % fold.y
+				g.field[point{k.x, py}] = struct{}{}
+				delete(g.field, k)
+			}
 		}
 	}
-	g.h -= y + 1
-}
-
-func (g *grid) foldX(x int) {
-	for k := range g.field {
-		if k.x > x {
-			px := (x - (k.x % x)) % x
-			g.field[point{px, k.y}] = struct{}{}
-			delete(g.field, k)
-		}
-		g.w -= x + 1
+	if fold.x != -1 {
+		g.w -= fold.x
+	} else {
+		g.h -= fold.y
 	}
 }
 
-func solve(file io.Reader) (answer int) {
-	xs, ys := make([]int, 0, 10), make([]int, 0, 10)
+func solve(file io.Reader) {
+	folds, g := parse(file)
+	for i, f := range folds {
+		if i == 1 {
+			fmt.Printf("First answer: %d\n", len(g.field))
+		}
+		g.fold(f)
+	}
+	fmt.Println("Second answer:")
+	g.String()
+}
+
+func parse(file io.Reader) ([]fold, grid) {
+	folds := make([]fold, 0, 20)
 	mx, my := 0, 0
 	g := grid{field: make(map[point]struct{})}
 	scanner := bufio.NewScanner(file)
@@ -79,10 +84,11 @@ func solve(file io.Reader) (answer int) {
 		if strings.HasPrefix(scanner.Text(), "fold") {
 			s := strings.Split(scanner.Text(), "=")
 			if strings.HasSuffix(s[0], "x") {
-				xs = append(xs, helpers.MustAtoi(s[1]))
+				folds = append(folds, fold{helpers.MustAtoi(s[1]), -1})
+
 				continue
 			}
-			ys = append(ys, helpers.MustAtoi(s[1]))
+			folds = append(folds, fold{-1, helpers.MustAtoi(s[1])})
 			continue
 		}
 		s := strings.Split(scanner.Text(), ",")
@@ -99,12 +105,7 @@ func solve(file io.Reader) (answer int) {
 	}
 	g.h = my
 	g.w = mx
-	log.Println(g)
-	log.Println(xs)
-	log.Println(ys)
-	g.foldX(655)
-	answer = len(g.field)
-	return answer
+	return folds, g
 }
 
 func main() {
@@ -115,6 +116,5 @@ func main() {
 	defer func(file *os.File) {
 		_ = file.Close()
 	}(file)
-	a1 := solve(file)
-	fmt.Printf("First answer: %d\n", a1)
+	solve(file)
 }
