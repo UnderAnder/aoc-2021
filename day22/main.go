ef15fc50280e62a730a47a6628c3c47846ffbaf2
Under Ander <andryushin.alexander@gmail.com>
2021-12-23 05:07:11 +0700

    part1 simplify

--- day22/main.go
+++ day22/main.go
@@ -7,6 +7,13 @@ import (
 	"aoc/pkg/helpers"
 )
 
+func inOrder(a, b int) (int, int) {
+	if a > b {
+		return b, a
+	}
+	return a, b
+}
+
 func min(a, b int) int {
 	if b < a {
 		return b
@@ -21,13 +28,6 @@ func max(a, b int) int {
 	return a
 }
 
-func inOrder(a, b int) (int, int) {
-	if a > b {
-		return b, a
-	}
-	return a, b
-}
-
 type cube struct {
 	x, y, z int
 }
@@ -36,10 +36,6 @@ type cuboid struct {
 	x1, x2 int
 	y1, y2 int
 	z1, z2 int
-}
-
-type Instruction struct {
-	cuboid cuboid
 	on     bool
 }
 
@@ -52,9 +48,9 @@ func part1(input []string) (answer1 int) {
 	cubesState := make(map[cube]bool)
 
 	for _, step := range steps {
-		for x := max(-50, step.cuboid.x1); x <= min(50, step.cuboid.x2); x++ {
-			for y := max(-50, step.cuboid.y1); y <= min(50, step.cuboid.y2); y++ {
-				for z := max(-50, step.cuboid.z1); z <= min(50, step.cuboid.z2); z++ {
+		for x := max(-50, step.x1); x <= min(50, step.x2); x++ {
+			for y := max(-50, step.y1); y <= min(50, step.y2); y++ {
+				for z := max(-50, step.z1); z <= min(50, step.z2); z++ {
 					cubesState[cube{x, y, z}] = step.on
 				}
 			}
@@ -70,7 +66,7 @@ func part1(input []string) (answer1 int) {
 	return len(cubesState)
 }
 
-func parse(input []string) (instructions []Instruction) {
+func parse(input []string) (cuboids []cuboid) {
 	for _, step := range input {
 		s := strings.Split(step, " ")
 		str := strings.Split(s[1], ",")
@@ -81,12 +77,10 @@ func parse(input []string) (instructions []Instruction) {
 		y1, y2 := inOrder(helpers.MustAtoi(y[0]), helpers.MustAtoi(y[1]))
 		z1, z2 := inOrder(helpers.MustAtoi(z[0]), helpers.MustAtoi(z[1]))
 
-		cuboid := cuboid{x1, x2, y1, y2, z1, z2}
-		instruction := Instruction{cuboid, s[0] == "on"}
-
-		instructions = append(instructions, instruction)
+		cuboids = append(cuboids, cuboid{x1, x2, y1, y2, z1, z2, s[0] == "on"})
 	}
-	return instructions
+
+	return cuboids
 }
 
 func main() {
