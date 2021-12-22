9e7acf2faaf2dad070e372ab5cfc703f01663747
Under Ander <andryushin.alexander@gmail.com>
2021-12-23 05:19:41 +0700

    add day22 part2

--- day22/main.go
+++ day22/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
@@ -41,6 +42,58 @@ type cuboid struct {
 
 func (c *cuboid) volume() int {
 	return (c.x2 - c.x1 + 1) * (c.y2 - c.y1 + 1) * (c.z2 - c.z1 + 1)
+}
+
+func (c *cuboid) intersection(c2 cuboid) (*cuboid, error) {
+	x1, x2 := max(c.x1, c2.x1), min(c.x2, c2.x2)
+	y1, y2 := max(c.y1, c2.y1), min(c.y2, c2.y2)
+	z1, z2 := max(c.z1, c2.z1), min(c.z2, c2.z2)
+	if x2 < x1 || y2 < y1 || z2 < z1 {
+		return nil, errors.New("intersection not found")
+	}
+	intersection := cuboid{x1, x2, y1, y2, z1, z2, false}
+	return &intersection, nil
+}
+
+func getIntersections(c cuboid, cuboids []cuboid) []cuboid {
+	intersections := make([]cuboid, 0, len(cuboids)+1)
+	if c.on {
+		intersections = append(intersections, c)
+	}
+
+	for _, cuboid := range cuboids {
+		if intersection, err := c.intersection(cuboid); err == nil {
+			intersection.on = !cuboid.on
+			intersections = append(intersections, *intersection)
+		}
+	}
+
+	return intersections
+}
+
+func count(cuboids []cuboid) (cnt int) {
+	for _, c := range cuboids {
+		if c.on {
+			cnt += c.volume()
+		} else {
+			cnt -= c.volume()
+		}
+	}
+	return cnt
+}
+
+func part2(input []string) int {
+	cuboids := parse(input)
+	all := make([]cuboid, 0)
+
+	for _, c := range cuboids {
+		intersections := getIntersections(c, all)
+		all = append(all, intersections...)
+	}
+
+	cubesOn := count(all)
+
+	return cubesOn
 }
 
 func part1(input []string) (answer1 int) {
@@ -86,5 +139,7 @@ func parse(input []string) (cuboids []cuboid) {
 func main() {
 	input := "input.txt"
 	a1 := part1(helpers.LoadString(input))
+	a2 := part2(helpers.LoadString(input))
 	fmt.Println("First answer: ", a1)
+	fmt.Println("Second answer: ", a2)
 }
